58b24cd8bd6d6fd5fd1b8357fbe2242a78223978
cpustejovsky <charles.pustejovsky@gmail.com>
2019-08-07 08:31:04 -0400

    Add models directory with User model and connected to app.js Set up mongoose to save to localdb and now having first argument saving as a user's username and second argument as a user's password. TODO: need to be secured

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
+//IMPORTS
 const Express = require("express");
 const app = Express();
 const mongoose = require("mongoose");
+const User = require("./models/user.js");
 
+//VARIABLES
 let port = (process.env.PORT = 3000);
+let username = process.argv[2];
+let password = process.argv[3];
 
 mongoose
+  /* Setup to deal with deprecation warnings */
+  .set("useCreateIndex", true)
+  .set("useNewUrlParser", true)
   .connect("mongodb://localhost:27017/CharlesPustejovsky-2019")
-  .then(() => console.log("connected to database"))
+  .then(() => {
+    console.log("connected to database");
+    // authenticate to server with a password: ability to set password for user via argument when starting server
+    console.log(username);
+    console.log(password);
+
+    if (username && password) {
+      let user = new User({
+        username: username,
+        password: password
+      });
+      user.save(err => {
+        if (err) {
+          let error = "Something bad happened! Please try again!";
+
+          if ((err.code = 11000)) {
+            error = "That email is already taken, please try another.";
+          }
+          console.log(error);
+        }
+        console.log(user);
+      });
+    } else {
+      console.log(
+        "try again and provide a username as the first argument and a password as the second argument"
+      );
+    }
+  })
   .catch(err => console.log(err));
 
-app.listen(port, () => console.log(`Running on Port ${port}`));
-
-// authenticate to server with a password: ability to set password for user via argument when starting server
+app.listen(port, () => {
+  console.log(`Running on Port ${port}`);
+});
 
 // store public key to server
 
