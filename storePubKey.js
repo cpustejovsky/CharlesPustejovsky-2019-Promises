84684c3d16013c947881e28c2caa4478e68d942a
cpustejovsky <charles.pustejovsky@gmail.com>
2019-08-09 21:23:53 -0400

    started trying to replace callbacks with Promises

--- storePubKey.js
+++ storePubKey.js
@@ -8,7 +8,10 @@ const User = require("./models/user.js");
 let username = process.argv[2];
 let password = process.argv[3];
 let pubKey = process.argv[4];
-
+const logErrorAndExit = errMsg => {
+  console.log(errMsg);
+  process.exit(1);
+};
 mongoose
   /* Setup to deal with deprecation warnings */
   .set("useCreateIndex", true)
@@ -18,52 +21,47 @@ mongoose
     //find User by username and compare password provided to the hash stored on mongodb
     if (username && password && pubKey) {
       console.log("authenticating user...");
-      User.findOne({ username: username }, (err, user) => {
-        //wasn't specifically catching the error of no username match and instead failing on
-        //`can't read property password of null` so I set this up for clearer error handling
-        if (err || !user) {
-          if (err) throw err;
-          if (!user) console.log(`User not found.`);
-          process.exit(1);
-        } else {
-          console.log(`found user!`);
-          console.log(`authenticating your password...`);
-          if (bcrypt.compareSync(password, user.password)) {
-            //TODO: replace with Promise
-            fs.readFile(pubKey, (err, data) => {
-              if (err) throw err;
-              else {
-                parsedData = data.toString();
-                user.publicKey = parsedData;
-                user.save(err => {
-                  if (err) {
-                    console.log(err);
-                    process.exit(1);
-                  } else {
-                    console.log(
-                      `successfully saved data from ${pubKey} to ${
-                        user.username
-                      }`
-                    );
-                    process.exit(0);
-                  }
-                });
-              }
-            });
+      const authenticatedUser = new Promise((resolve, reject) => {
+        User.findOne({ username: username }, (err, user) => {
+          if (err) {
+            reject(err);
+          } else if (!user) {
+            reject("User not found.");
           } else {
-            console.log(`passwords did not match. please try again.`);
-            process.exit(1);
+            console.log(`found user!`);
+            resolve(user);
           }
+        });
+      });
+      const correctPassword = new Promise((resolve, reject) => {
+        if (!bcrypt.compareSync(password, user.password)) {
+          reject();
+        } else {
+          fs.readFile(pubKey, (err, data) => {
+            if (err) logErrorAndExit(err);
+            else {
+              parsedData = data.toString();
+              user.publicKey = parsedData;
+              user.save(err => {
+                if (err) {
+                  logErrorAndExit(err);
+                } else {
+                  console.log(
+                    `successfully saved data from ${pubKey} to ${user.username}`
+                  );
+                  process.exit(0);
+                }
+              });
+            }
+          });
         }
       });
     } else {
-      console.log(
-        "try again and provide a username as the first argument, a password as the second argument, and a pubic key you want to store as the third argument"
+      logErrorAndExit(
+        "try again and provide a username as the first argument, a password as the second, and a pubic key you want to store as the third"
       );
-      process.exit(1);
     }
   })
   .catch(err => {
-    console.log(err);
-    process.exit(1);
+    logErrorAndExit(err);
   });
