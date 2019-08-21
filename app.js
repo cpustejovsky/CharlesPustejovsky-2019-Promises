64b66a7c44cccdd5476a99ac9c9ab033f377ae3d
cpustejovsky <cpustejovsky@bitpay.com>
2019-08-21 16:21:48 -0400

    setting up home page views

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const Express = require("express");
 const app = Express();
 const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app
   .listen(port, () => {
     console.log(`Running on port ${port}`);
